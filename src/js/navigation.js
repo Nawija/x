32cbfae37ed247fc8a0db78bdb8a7c218b2b6a1f
Mebloo <konradwiel@interia.pl>
2023-01-26 18:30:04 +0100

    X

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -5,8 +5,8 @@ const priceSection = document.querySelectorAll(".priceSection");
 const burger = document.getElementById("burger");
 
 // svg icons burger and close
-const b = document.querySelector('.b')
-const x = document.querySelector('.x')
+const b = document.querySelector(".b");
+const x = document.querySelector(".x");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY >= 100) {
@@ -20,25 +20,22 @@ for (const link of priceSection) {
     link.addEventListener("click", scrollToSection);
 }
 
-
 function scrollToSection(e) {
     e.preventDefault();
     const href = this.getAttribute("href");
     const offsetTop = document.querySelector(href).offsetTop;
     const headerHeight = document.querySelector("#header").offsetHeight;
-  
+
     scroll({
-      top: offsetTop - headerHeight,
-      behavior: "smooth"
+        top: offsetTop - headerHeight,
+        behavior: "smooth",
     });
-  }
-  
-  burger.addEventListener('click',()=>{
-        x.classList.toggle('hidden')
-        b.classList.toggle('hidden')
-    })
+}
 
-
+burger.addEventListener("click", () => {
+    x.classList.toggle("hidden");
+    b.classList.toggle("hidden");
+});
 
 burger.addEventListener("click", () => {
     if (navList.classList.contains("-top-[400%]")) {
@@ -46,11 +43,15 @@ burger.addEventListener("click", () => {
         navList.classList.remove("-top-[400%]");
         navList.classList.add("opacity-100");
         navList.classList.remove("opacity-0");
+        x.classList.remove("hidden");
+        b.classList.add("hidden");
     } else {
         navList.classList.remove("top-[100%]");
         navList.classList.add("-top-[400%]");
         navList.classList.remove("opacity-100");
         navList.classList.add("opacity-0");
+        x.classList.add("hidden");
+        b.classList.remove("hidden");
     }
     navList.classList.toggle("shadow-lg");
     allLinks.forEach((item) => {
@@ -59,6 +60,8 @@ burger.addEventListener("click", () => {
             navList.classList.remove("top-[100%]");
             navList.classList.remove("opacity-100");
             navList.classList.add("opacity-0");
+            x.classList.add("hidden");
+            b.classList.remove("hidden");
         });
     });
 });
